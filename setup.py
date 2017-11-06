5d0e9b4943bc69caa44924b681df9f920c4b2260
MysterialPy <mysterialpy@gmail.com>
2017-11-06 18:35:59 +1000

    Added setup and requirements files.
    Signed-off-by: MysterialPy <mysterialpy@gmail.com>

---
+++ setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+import re, os
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = ''
+with open('twitchio/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+readme = ''
+with open('README.md') as f:
+    readme = f.read()
+
+setup(name='twitchio',
+      author='MysterialPy',
+      url='https://github.com/MysterialPy/TwitchIO',
+      version=version,
+      packages=['twitchio', 'twitchio.commands'],
+      license='MIT',
+      description='A python IRC and API wrapper for Twitch.',
+      long_description=readme,
+      include_package_data=True,
+      install_requires=requirements,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
+)
---
+++ twitchio/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+"""
+TwitchIO
+~~~~~~~~~~~~~~~~~~~
+A Twitch IRC Bot and API Wrapper.
+:copyright: (c) 2017 MysterialPy
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'twitchio'
+__author__ = 'MysterialPy'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 MysterialPy'
+__version__ = '0.0.1a'
+
+from .abcs import Messageable
+from .client import Client
+from .connection import *
+from .dataclasses import Context, Message, User, Channel
+from .errors import *
