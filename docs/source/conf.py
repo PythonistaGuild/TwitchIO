636cc65062d2378f432dd5f13c0bd5de15e10e3c
MysterialPy <mysterialpy@gmail.com>
2017-11-09 03:43:12 +1000

    Added Readthedocs.yml
    Signed-off-by: MysterialPy <mysterialpy@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ html_logo = 'logo.png'
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.6.5'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
