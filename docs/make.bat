0335d8fca526ab723d40eb7b2fa6edeeb3f02a30
MysterialPy <mysterialpy@gmail.com>
2017-11-09 04:37:11 +1000

    ?RTD wtf
    Signed-off-by: MysterialPy <mysterialpy@gmail.com>

--- docs/make.bat
+++ docs/make.bat
@@ -10,7 +10,6 @@ if "%SPHINXBUILD%" == "" (
 set SOURCEDIR=source
 set BUILDDIR=build
 set SPHINXPROJ=TwitchIO
-set SPHINXOPTS=-E
 
 if "%1" == "" goto help
 
@@ -27,7 +26,7 @@ if errorlevel 9009 (
 	exit /b 1
 )
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %-E%
+%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 goto end
 
 :help
