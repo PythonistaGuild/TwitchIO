be346a0101d8343a1035e0468f45aee07125c2ae
MysterialPy <mysterialpy@gmail.com>
2017-11-11 06:19:22 +1000

    Fixes to requirements and added GIT Commit to version number, for installs made by git.
    Signed-off-by: MysterialPy <mysterialpy@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,40 @@
 from setuptools import setup
 import re, os
 
+on_rtd = os.getenv('READTHEDOCS') == 'True'
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
+
+if on_rtd:
+    requirements.append('sphinxcontrib-napoleon')
+    requirements.append('sphinxcontrib-asyncio')
+    requirements.append('sphinxcontrib-websupport')
 
 version = ''
 with open('twitchio/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
+
+# Thanks Dannyo(Rapptz) again...
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+' + out.decode('utf-8').strip()
+    except Exception:
+        pass
 
 if not version:
     raise RuntimeError('version is not set')
