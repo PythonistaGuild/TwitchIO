147fbcfefbfab7c1fab5f1b9672c8999922fcd48
SnowyLuma <SnowyLuma@gmail.com>
2018-10-10 02:41:52 +0200

    Better pagination and ratelimit handling for Helix APIs

--- twitchio/client.py
+++ twitchio/client.py
@@ -1,13 +1,13 @@
 import asyncio
 from typing import Union
-from twitchio.http import HTTPSession
+from twitchio.http import HelixHTTPSession
 
 
 class TwitchClient:
 
     def __init__(self, *, loop=None, client_id=None, **kwargs):
         loop = loop or asyncio.get_event_loop()
-        self.http = HTTPSession(loop=loop, client_id=client_id)
+        self.http = HelixHTTPSession(loop=loop, client_id=client_id)
 
     async def get_users(self, *users: Union[str, int]):
         """|coro|
@@ -34,7 +34,8 @@ class TwitchClient:
         .. note::
             This method accepts both user ids or names, or a combination of both. Multiple names/ids may be passed.
         """
-        return await self.http._get_users(*users)
+
+        return await self.http.get_users(*users)
 
     async def get_stream_by_name(self, channel: str):
         """|coro|
@@ -56,7 +57,9 @@ class TwitchClient:
         TwitchHTTPException
             Bad request while fetching stream.
         """
-        return await self.http._get_stream_by_name(channel)
+
+        data = await self.http.get_streams(channels=[channel])
+        return data[0]
 
     async def get_stream_by_id(self, channel: int):
         """|coro|
@@ -78,17 +81,25 @@ class TwitchClient:
         TwitchHTTPException
             Bad request while fetching stream.
         """
-        return await self.http._get_stream_by_id(channel)
 
-    async def get_streams(self, *channels: Union[int, str]):
+        data = await self.http.get_streams(channels=[channel])
+        return data[0]
+
+    async def get_streams(self, *, game_id=None, language=None, channels=None, limit=None):
         """|coro|
 
         Method which retrieves multiple stream information on the given channels, provided they are active (Live).
 
         Parameters
         ------------
-        \*channels: Union[int, str]
+        game_id: Optional[int]
+            The game to filter streams for.
+        language: Optional[str]
+            The language to filter streams for.
+        channels: Union[int, str]
             The channels in id or name form, to retrieve information for.
+        limit: Optional[int]
+            Maximum number of results to return.
 
         Returns
         ---------
@@ -100,7 +111,8 @@ class TwitchClient:
         TwitchHTTPException
             Bad request while fetching streams.
         """
-        return await self.http._get_streams(*channels)
+
+        return await self.http.get_streams(game_id=game_id, language=language, channels=channels, limit=limit)
 
     async def get_games(self, *games: Union[str, int]):
         """|coro|
@@ -122,7 +134,31 @@ class TwitchClient:
         TwitchHTTPException
             Bad request while fetching games.
         """
-        return await self.http._get_games(*games)
+
+        return await self.http.get_games(*games)
+
+    async def get_top_games(self, limit=None):
+        """|coro|
+
+        Retrieves the top games currently being played on Twitch.
+
+        Parameters
+        ------------
+        limit: Optional[int]
+            Maximum amount of results to fetch.
+
+        Returns
+        ---------
+        list:
+            List containing game information. Could be None if no games matched.
+
+        Raises
+        --------
+        TwitchHTTPException
+            Bad request while fetching games.
+        """
+
+        return await self.http.get_top_games(limit=limit)
 
     async def get_chatters(self, channel: str):
         """|coro|
@@ -144,4 +180,5 @@ class TwitchClient:
         TwitchHTTPException
             Bad request while fetching stream chatters.
         """
-        return await self.http._get_chatters(channel)
+
+        raise NotImplementedError
