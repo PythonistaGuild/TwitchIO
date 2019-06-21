1eae09e70e7b038fd0dafa55aabeb6e5c1f6e77c
Eviee <evieepy@gmail.com>
2019-06-21 11:33:16 +1000

    Added setup and requirements files.

---
+++ setup.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+"""
+The MIT License (MIT)
+
+Copyright (c) 2017-2019 TwitchIO
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+import os
+from setuptools import setup
+
+
+on_rtd = os.getenv('READTHEDOCS') == 'True'
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+if on_rtd:
+    requirements.append('sphinx==1.7.4')
+    requirements.append('sphinxcontrib-napoleon')
+    requirements.append('sphinxcontrib-asyncio')
+    requirements.append('sphinxcontrib-websupport')
+    requirements.append('Pygments')
+
+version = '2.0.0a1'
+
+readme = ''
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(name='twitchio',
+      author='TwitchIO',
+      url='https://github.com/TwitchIO/TwitchIO',
+      version=version,
+      packages=['twitchio', 'twitchio.ext.commands'],
+      license='MIT',
+      description='A Python IRC and API wrapper for Twitch.',
+      long_description=readme,
+      include_package_data=True,
+      install_requires=requirements,
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
+)
