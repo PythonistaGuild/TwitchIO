2184cf9e0ecf7f64f8b4d5f243b051df0b156c57
Saints1789 <34809615+Saints1789@users.noreply.github.com>
2020-11-22 20:07:04 +0800

    修復預測系統造成的問題 (Line 179)

--- twitchio/dataclasses.py
+++ twitchio/dataclasses.py
@@ -176,7 +176,7 @@ class User:
         if badges:
             for chunk in badges.split(','):
                 k, _, v = chunk.partition('/')
-                self._badges[k] = int(v)
+                self._badges[k] = v # int(v)
 
         self._mod = self._tags.get('mod', 0) if self._tags else attrs.get('mod', 0)
 
