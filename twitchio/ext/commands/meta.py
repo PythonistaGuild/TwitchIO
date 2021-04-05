378beba5af5841a1e332dda9df469038723c9edb
Myst(MysterialPy) <mysterialpy@gmail.com>
2021-04-05 22:30:57 +1000

    Basic Cog documentation.

--- twitchio/ext/commands/meta.py
+++ twitchio/ext/commands/meta.py
@@ -33,14 +33,54 @@ __all__ = ("Cog",)
 
 
 class Cog:
+    """Class used for creating a TwitchIO Cog.
 
-    __valid_slots__ = ('cog_unload', 'cog_check', 'cog_error', 'cog_command_error')
+    Cogs help organise code and provide powerful features for creating bots.
+    Cogs can contain commands, events and special cog specific methods to help with checks,
+    before and after command invocation hooks, and cog error handlers.
+
+    To use a cog simply subclass Cog and add it. Once added, cogs can be un-added and re-added live.
+
+    Examples
+    ----------
+
+    .. code:: py
+
+        # In modules/test.py
+
+        from twitchio.ext import commands
+
+
+        class MyCog(commands.Cog):
+
+            def __init__(self, bot: commands.Bot):
+                self.bot = bot
+
+            @commands.command()
+            async def hello(self, ctx: commands.Context):
+                await ctx.send(f"Hello, {ctx.author.name}!")
+
+            @commands.Cog.event()
+            async def event_message(self, message):
+                # An event inside a cog!
+                if message.echo:
+                    return
+
+                print(message.content)
+
+
+        def prepare(bot: commands.Bot):
+            # Load our cog with this module...
+            bot.add_cog(MyCog(bot))
+    """
+
+    __valid_slots__ = ("cog_unload", "cog_check", "cog_error", "cog_command_error")
 
     def __init_subclass__(cls, *args, **kwargs):
         cls.__cogname__ = kwargs.get("name", cls.__name__)
 
         for name, mem in inspect.getmembers(cls):
-            if name.startswith(('cog', 'bot')) and name not in cls.__valid_slots__:  # Invalid method prefixes
+            if name.startswith(("cog", "bot")) and name not in cls.__valid_slots__:  # Invalid method prefixes
                 raise InvalidCogMethod(f'The method "{name}" starts with an invalid prefix (cog or bot).')
 
         cls._events = getattr(cls, "_events", {})
@@ -87,6 +127,26 @@ class Cog:
 
     @classmethod
     def event(cls, event: str = None):
+        """Add an event listener to this Cog.
+
+        Examples
+        ----------
+
+        .. code:: py
+
+            class MyCog(commands.Cog):
+
+                def __init__(...):
+                    ...
+
+                @commands.Cog.event()
+                async def event_message(self, message: twitchio.Message):
+                    print(message.content)
+
+                @commands.Cog.event("event_ready")
+                async def bot_is_ready(self):
+                    print('Bot is ready!')
+        """
         if not hasattr(cls, "_events"):
             cls._events = {}
 
@@ -101,23 +161,49 @@ class Cog:
                 cls._events[event_name] = [func]
 
             return func
+
         return decorator
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """This cogs name."""
         return self.__cogname__
 
     @property
-    def commands(self):
+    def commands(self) -> dict:
+        """The commands associated with this cog as a mapping."""
         return self._commands
 
     async def cog_error(self, exception: Exception):
         pass
 
-    async def cog_command_error(self, ctx, exception: Exception):
+    async def cog_command_error(self, ctx: Context, exception: Exception):
+        """Method invoked when an error is raised in one of this cogs commands.
+
+        Parameters
+        -------------
+        ctx: :class:`Context`
+            The context around the invoked command.
+        exception: Exception
+            The exception raised.
+        """
         pass
 
     async def cog_check(self, ctx: Context) -> bool:
+        """A cog-wide check which is ran everytime a command from this Cog is invoked.
+
+        Parameters
+        ------------
+        ctx: :class:`Context`
+            The context used to try and invoke this command.
+
+        Notes
+        -------
+        .. note::
+
+            This method must return True/False or raise. If this check returns False or raises, it will fail
+            and an exception will be propagated to error handlers.
+        """
         return True
 
     def cog_unload(self):
