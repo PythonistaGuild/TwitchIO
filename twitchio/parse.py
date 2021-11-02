50509a39aa1260c9098158b4110d977e5fb4fe4e
chillymosh <86857777+chillymosh@users.noreply.github.com>
2021-11-02 14:54:44 +0000

    Fix echo for ctx.reply
    echo on ctx.reply is now treated correctly

--- twitchio/parse.py
+++ twitchio/parse.py
@@ -77,6 +77,12 @@ def parser(data: str, nick: str):
     elif action in ACTIONS:
         channel = groups[-1].lstrip("#")
 
+    elif groups[3] in {"PRIVMSG", "PRIVMSG(ECHO)"}:
+        action = groups[3]
+        channel = groups[4].lstrip("#")
+        message = " ".join(groups[5:]).lstrip(":")
+        user = re.search(USER_SUB, groups[2]).group("user")
+
     if action in ACTIONS2:
         prebadge = groups[0].split(";")
         badges = {}
