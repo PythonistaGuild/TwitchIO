13d34e517d3a07c16bc95be7fe5c3ec92f8c98b2
Myst(MysterialPy) <evieepy@gmail.com>
2021-11-29 01:53:56 +1000

    Fix error in edit_version workflow

--- .github/workflows/edit_version.py
+++ .github/workflows/edit_version.py
@@ -10,7 +10,7 @@ args = parser.parse_args()
 with open("../../twitchio/__init__.py", "r+") as fp:
     data = fp.read()
 
-    data = re.sub(r"__version__ = \"\d+.\d+.\d+\"", f'__version__ = "{args["latest"].removeprefix("v")}"', data)
+    data = re.sub(r"__version__ = \"\d+.\d+.\d+\"", f'__version__ = "{args.latest.removeprefix("v")}"', data)
 
     fp.seek(0)
     fp.write(data)
