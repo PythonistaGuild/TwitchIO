d0cc86eb4b2c60ea871d1bf6ba5b64b6bdc83233
Eviee <evieepy@gmail.com>
2022-02-06 06:10:34 +1000

    Added properties to ShardInfo, instead of overwritable attributes.

--- twitchio/shards.py
+++ twitchio/shards.py
@@ -9,6 +9,26 @@ class ShardInfo:
     def __init__(self, **kwargs):
         self._websocket: Websocket = kwargs.get('websocket')
 
-        self.number: int = kwargs.get('number')
-        self.channels: list = kwargs.get('channels')
-        self.ready: bool = False
+        self._number: int = kwargs.get('number')
+        self._channels: list = kwargs.get('channels')
+        self._ready: bool = False
+
+    @property
+    def number(self) -> int:
+        """Returns the Shard number."""
+        return self._number
+
+    @property
+    def index(self) -> int:
+        """An alias to :func:`number`."""
+        return self._number
+
+    @property
+    def channels(self) -> list:
+        """Returns the channels associated with the Shard."""
+        return self._channels
+
+    @property
+    def ready(self) -> bool:
+        """Returns a bool indicating whether the Shard is in a ready state."""
+        return self._ready
