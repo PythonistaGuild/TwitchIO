PythonistaGuild/TwitchIO

commit 28186729621770a788c4596eff9b563a0dc3b8b0

Add user_id property to client (#256)

twitchio/http.py
@@ -98,6 +98,7 @@ class TwitchHTTP:
         self.client_secret = client_secret
         self.client_id = client_id
         self.nick = None
+        self.user_id = None
 
         self.bucket = RateBucket(method="http")
         self.scopes = kwargs.get("scopes", [])
@@ -325,6 +326,7 @@ class TwitchHTTP:
 
         if not self.nick:
             self.nick = data.get("login")
+            self.user_id = int(data.get("user_id"))
             self.client_id = data.get("client_id")
 
         return data

twitchio/client.py
@@ -336,6 +336,11 @@ class Client:
         """The IRC bots nick."""
         return self._http.nick or self._connection.nick
 
+    @property
+    def user_id(self):
+        """The IRC bot user id."""
+        return self._http.user_id or self._connection.user_id
+
     def create_user(self, user_id: int, user_name: str) -> PartialUser:
         """
         A helper method to create a :class:`twitchio.PartialUser` from a user id and user name.

twitchio/websocket.py
@@ -90,6 +90,7 @@ class WSConnection:
         }
 
         self.nick = None
+        self.user_id = None
         self._token = token
         self.modes = modes or ("commands", "tags", "membership")
         self._initial_channels = initial_channels or []
@@ -126,6 +127,7 @@ class WSConnection:
         if not self._client._http.nick:
             data = await self._client._http.validate(token=self._token)
             self.nick = data["login"]
+            self.user_id = int(data["user_id"])
 
         session = self._client._http.session
 

commit ccea9ed0a06ffcdfa96d9729d0d34297db318872

fix issue with app tokens caused by PR #256

twitchio/http.py
@@ -26,7 +26,7 @@ import asyncio
 import copy
 import datetime
 import logging
-from typing import TYPE_CHECKING, Union, List, Tuple, Any, Dict
+from typing import TYPE_CHECKING, Union, List, Tuple, Any, Dict, Optional
 
 import aiohttp
 from yarl import URL
@@ -98,7 +98,7 @@ class TwitchHTTP:
         self.client_secret = client_secret
         self.client_id = client_id
         self.nick = None
-        self.user_id = None
+        self.user_id: Optional[int] = None
 
         self.bucket = RateBucket(method="http")
         self.scopes = kwargs.get("scopes", [])
@@ -328,7 +328,7 @@ class TwitchHTTP:
 
         if not self.nick:
             self.nick = data.get("login")
-            self.user_id = int(data.get("user_id"))
+            self.user_id = data.get("user_id") and int(data["user_id"])
             self.client_id = data.get("client_id")
 
         return data