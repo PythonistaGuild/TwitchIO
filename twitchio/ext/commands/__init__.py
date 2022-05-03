d36906bfb42b97a346dbc2a1b3a84d54ddcf17d6
Eviee <evieepy@gmail.com>
2022-05-03 12:55:29 +1000

    Remove old components init and add it to commands extension.

--- twitchio/ext/commands/__init__.py
+++ twitchio/ext/commands/__init__.py
@@ -1,3 +1,3 @@
 from .bot import Bot
-from .commands import Command as command
-from .commands import Collection as collection
+from .commands import Command, command
+from .components import Component
