16b76f3b43d47a465c37f46528cf3b7dc2961580
chillymosh <86857777+chillymosh@users.noreply.github.com>
2022-05-24 20:53:54 +0100

    Update copyright

--- twitchio/errors.py
+++ twitchio/errors.py
@@ -1,7 +1,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2017-2021 TwitchIO
+Copyright (c) 2017-present TwitchIO
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
