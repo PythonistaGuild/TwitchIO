10271af5972e3f4a6973f87bef57cee7d2eb0437
dnns01 <github@dnns01.de>
2022-12-13 17:18:55 +0100

    Fix parsing ScheduleSegments with no End Time set

--- twitchio/models.py
+++ twitchio/models.py
@@ -1279,7 +1279,7 @@ class ScheduleSegment:
     def __init__(self, data: dict):
         self.id: str = data["id"]
         self.start_time = parse_timestamp(data["start_time"])
-        self.end_time = parse_timestamp(data["end_time"])
+        self.end_time = parse_timestamp(data["end_time"]) if data["end_time"] else None
         self.title: str = data["title"]
         self.canceled_until = parse_timestamp(data["canceled_until"]) if data["canceled_until"] else None
         self.category = ScheduleCategory(data["category"]) if data["category"] else None
