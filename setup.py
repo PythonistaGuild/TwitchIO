6efb9360f0eb936dd87111252ca59276ef3fd3d3
IAmTomahawkx <iamtomahawkx@gmail.com>
2023-03-18 17:59:07 -0700

    update python version in pypi tags

--- setup.py
+++ setup.py
@@ -86,6 +86,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
