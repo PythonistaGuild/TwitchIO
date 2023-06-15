f937984d113a8c205283d342351c9d1fbe000e55
jmcb <joelsgp@protonmail.com>
2023-06-15 22:07:46 +0100

    Remove old __future__ imports

--- twitchio/ext/commands/meta.py
+++ twitchio/ext/commands/meta.py
@@ -22,7 +22,6 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-from __future__ import annotations
 import inspect
 import types
 from functools import partial
