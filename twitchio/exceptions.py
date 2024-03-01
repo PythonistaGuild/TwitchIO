944321ee53d510504dd3442a15ae71d51d690dac
EvieePy <evieepy@gmail.com>
2024-03-02 05:46:04 +1000

    Add Invalid token exception and extras to HTTPException

--- twitchio/exceptions.py
+++ twitchio/exceptions.py
@@ -21,9 +21,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
+
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 
 __all__ = ("TwitchioException", "HTTPException")
@@ -45,7 +46,37 @@ class HTTPException(TwitchioException):
     # TODO: Document this class.
     # TODO: Add extra attributes to this class. E.g. response, status, etc.
 
-    def __init__(self, msg: str = "", /, *, route: Route | None = None, status: int) -> None:
+    def __init__(
+        self,
+        msg: str = "",
+        /,
+        *,
+        route: Route | None = None,
+        status: int,
+        extra: str | dict[str, Any],
+    ) -> None:
         self.route = route
         self.status = status
+        self.extra = {"message": extra} if isinstance(extra, str) else extra
+
         super().__init__(msg)
+
+
+class InvalidTokenException(HTTPException):
+    """Exception raised when an invalid token can not be validated or refreshed."""
+
+    def __init__(
+        self,
+        msg: str = "",
+        /,
+        *,
+        token: str | None = None,
+        refresh: str | None = None,
+        type_: str,
+        original: HTTPException,
+    ) -> None:
+        self.token = token
+        self.refresh = refresh
+        self.invalid_type = type_
+
+        super().__init__(msg, route=original.route, status=original.status, extra=original.extra)
