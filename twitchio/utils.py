fb269ed06dfae7ee038cf4c36dfc03ae9124cba9
EvieePy <evieepy@gmail.com>
2024-03-09 12:21:21 +1000

    Add logging formatter.

--- twitchio/utils.py
+++ twitchio/utils.py
@@ -1,4 +1,9 @@
 import json
+import logging
+import os
+import pathlib
+import sys
+from typing import Any
 
 
 try:
@@ -9,4 +14,126 @@ except ImportError:
     _from_json = json.loads
 
 
-__all__ = ("_from_json",)
+__all__ = ("_from_json", "setup_logging", "ColourFormatter", "ColorFormatter")
+
+
+def is_docker() -> bool:
+    path: pathlib.Path = pathlib.Path("/proc/self/cgroup")
+
+    exists: bool = path.exists()
+    is_file: bool = path.is_file()
+    return exists or (is_file and any("docker" in line for line in path.open()))
+
+
+def stream_supports_colour(stream: Any) -> bool:
+    is_a_tty = hasattr(stream, "isatty") and stream.isatty()
+
+    # Pycharm and Vscode support colour in their inbuilt editors
+    if "PYCHARM_HOSTED" in os.environ or os.environ.get("TERM_PROGRAM") == "vscode":
+        return is_a_tty
+
+    if sys.platform != "win32":
+        # Docker does not consistently have a tty attached to it
+        return is_a_tty or is_docker()
+
+    # ANSICON checks for things like ConEmu
+    # WT_SESSION checks if this is Windows Terminal
+    return is_a_tty and ("ANSICON" in os.environ or "WT_SESSION" in os.environ)
+
+
+def stream_supports_rgb(stream: Any) -> bool:
+    if not stream_supports_colour(stream):
+        return False
+
+    if "COLORTERM" in os.environ:
+        return os.environ["COLORTERM"] in ("truecolor", "24bit")
+
+    return False
+
+
+class ColourFormatter(logging.Formatter):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+
+        self._handler: logging.StreamHandler[Any] = kwargs.get("handler", logging.StreamHandler())
+
+        self._supports_colour: bool = stream_supports_colour(self._handler.stream)
+        self._supports_rgb: bool = stream_supports_rgb(self._handler.stream)
+
+        self._colours: dict[int, str] = {}
+        self._RESET: str = "\033[0m"
+
+        if self._supports_rgb:
+            self._colours = {
+                logging.DEBUG: "\x1b[40;1m",
+                logging.INFO: "\x1b[38;2;100;55;215m",
+                logging.WARNING: "\x1b[38;2;204;189;51m",
+                logging.ERROR: "\x1b[38;2;161;38;46m",
+                logging.CRITICAL: "\x1b[48;2;161;38;46;1m",
+            }
+
+        elif self._supports_colour:
+            self._colours = {
+                logging.DEBUG: "\x1b[40;1m",
+                logging.INFO: "\x1b[34;1m",
+                logging.WARNING: "\x1b[33;1m",
+                logging.ERROR: "\x1b[31m",
+                logging.CRITICAL: "\x1b[41;1m",
+            }
+
+        self._FORMATS: dict[int, logging.Formatter] = {
+            level: logging.Formatter(
+                f"\x1b[30;1m%(asctime)s\x1b[0m {colour}%(levelname)-8s\x1b[0m {colour}%(name)s\x1b[0m %(message)s"
+            )
+            for level, colour in self._colours.items()
+        }
+
+    def format(self, record: logging.LogRecord) -> str:
+        formatter: logging.Formatter | None = self._FORMATS.get(record.levelno, None)
+        if formatter is None:
+            formatter = self._FORMATS[logging.DEBUG]
+
+        # Override the traceback to always print in red
+        if record.exc_info:
+            text = formatter.formatException(record.exc_info)
+            record.exc_text = f"\x1b[31m{text}\x1b[0m"
+
+        output = formatter.format(record)
+
+        # Remove the cache layer
+        record.exc_text = None
+        return output
+
+
+ColorFormatter = ColourFormatter
+
+
+def setup_logging(
+    *,
+    handler: logging.Handler | None = None,
+    formatter: logging.Formatter | None = None,
+    level: int | None = None,
+    root: bool = True,
+) -> None:
+    if level is None:
+        level = logging.INFO
+
+    if handler is None:
+        handler = logging.StreamHandler()
+
+    if formatter is None:
+        if isinstance(handler, logging.StreamHandler) and stream_supports_colour(handler.stream):  # type: ignore
+            formatter = ColourFormatter()
+        else:
+            dt_fmt = "%Y-%m-%d %H:%M:%S"
+            formatter = logging.Formatter("[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{")
+
+    if root:
+        logger = logging.getLogger()
+    else:
+        library, _, _ = __name__.partition(".")
+        logger = logging.getLogger(library)
+
+    handler.setFormatter(formatter)
+    logger.setLevel(level)
+    logger.addHandler(handler)  # type: ignore
