b9a12d5c895fc95a566e396b69e4849cad7d9b1c
MystyPy <evieepy@gmail.com>
2024-04-18 02:53:05 +1000

    Add conduit types (Minus Events)

--- twitchio/types_/conduits.py
+++ twitchio/types_/conduits.py
@@ -44,3 +44,141 @@ class ShardData(TypedDict):
 class ConduitData(TypedDict):
     id: str
     shard_count: int
+
+
+class WelcomeMetaData(TypedDict):
+    message_id: str
+    message_type: Literal["session_welcome"]
+    message_timestamp: str
+
+
+class WelcomSession(TypedDict):
+    id: str
+    status: Literal["connected"]
+    keepalive_timeout_seconds: int
+    reconnect_url: None
+    connected_at: str
+
+
+class WelcomePayload(TypedDict):
+    session: WelcomSession
+
+
+class WelcomeMessage(TypedDict):
+    metadata: WelcomeMetaData
+    payload: WelcomePayload
+
+
+class KeepAliveMetaData(TypedDict):
+    message_id: str
+    message_type: Literal["session_keepalive"]
+    message_timestamp: str
+
+
+class KeepAliveMessage(TypedDict):
+    metadata: KeepAliveMetaData
+
+
+class NotificationMetaData(TypedDict):
+    message_id: str
+    message_type: Literal["notification"]
+    message_timestamp: str
+    subscription_type: str
+    subscription_version: str
+
+
+class Condition(TypedDict, total=False):
+    broadcaster_user_id: str
+    moderator_user_id: str
+    user_id: str
+    campaign_id: str
+    category_id: str
+    organization_id: str
+    client_id: str
+    conduit_id: str
+    reward_id: str
+    from_broadcaster_user_id: str
+    to_broadcaster_user_id: str
+    broadcaster_id: str
+
+
+class NotificationTransport(TypedDict):
+    method: Literal["websocket"]
+    session_id: str
+
+
+class NotificationSubscription(TypedDict):
+    id: str
+    status: Literal["enabled"]
+    type: str
+    version: str
+    cost: int
+    condition: Condition
+    transport: NotificationTransport
+    created_at: str
+
+
+class NotificationPayload(TypedDict):
+    subscription: NotificationSubscription
+    event: ...
+
+
+class NotificationMessage(TypedDict):
+    metadata: NotificationMetaData
+    payload: NotificationPayload
+
+
+class ReconnectMetadata(TypedDict):
+    message_id: str
+    message_str: Literal["session_reconnect"]
+    message_timestamp: str
+
+
+class ReconnectSession(TypedDict):
+    id: str
+    status: Literal["reconnecting"]
+    keepalive_timeout_seconds: None
+    reconnect_url: str
+    connected_at: str
+
+
+class ReconnectPayload(TypedDict):
+    session: ReconnectSession
+
+
+class ReconnectMessage(TypedDict):
+    metadata: ReconnectMetadata
+    payload: ReconnectPayload
+
+
+class RevocationMetaData(TypedDict):
+    message_id: str
+    message_type: Literal["revocation"]
+    message_timestamp: str
+    subscription_type: str
+    subscription_version: str
+
+
+class RevocationTransport(TypedDict):
+    method: Literal["websocket"]
+    session_id: str
+
+
+class RevocationSubscription(TypedDict):
+    id: str
+    status: Literal["authorization_revoked", "user_removed", "version_removed"]
+    type: str
+    version: str
+    cost: int
+    condition: Condition
+    transport: RevocationTransport
+    created_at: str
+
+
+class RevocationPayload(TypedDict):
+    subscription: RevocationSubscription
+
+
+class RevocationMessage(TypedDict):
+    metadata: RevocationMetaData
+    payload: ReconnectPayload
