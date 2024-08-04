6375d8b3412c9af36e90f0b3a4acc5d4c0f3cab0
chillymosh <86857777+chillymosh@users.noreply.github.com>
2024-08-04 12:39:07 +0100

    Eventuser User Payload Dicts

--- twitchio/types_/eventsub.py
+++ twitchio/types_/eventsub.py
@@ -22,8 +22,10 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from typing import Literal, TypedDict
+from typing import Generic, Literal, NotRequired, TypedDict, TypeVar
 
+
+T = TypeVar("T")
 
 EventSubHeaders = TypedDict(
     "EventSubHeaders",
@@ -38,3 +40,90 @@ EventSubHeaders = TypedDict(
     },
     total=False,
 )
+
+
+class ClientCondition(TypedDict):
+    client_id: str
+
+
+class UserCondition(TypedDict):
+    user_id: str
+
+
+class BroadcasterModeratorCondition(TypedDict):
+    broadcaster_user_id: str
+    moderator_user_id: str
+
+
+class Transport(TypedDict):
+    method: str
+    callback: str  # TODO check for websocket payloads
+
+
+class BaseSubscription(TypedDict, Generic[T]):
+    id: str
+    type: str
+    version: str
+    status: str
+    cost: NotRequired[int]
+    condition: T
+    transport: Transport
+    created_at: str
+
+
+class UserAuthorizationGrantEvent(TypedDict):
+    client_id: str
+    user_id: str
+    user_login: str
+    user_name: str
+
+
+class UserAuthorizationRevokeEvent(TypedDict):
+    client_id: str
+    user_id: str
+    user_login: str | None
+    user_name: str | None
+
+
+class UserAuthorizationGrantPayload(TypedDict):
+    subscription: BaseSubscription[ClientCondition]
+    event: UserAuthorizationGrantEvent
+
+
+class UserAuthorizationRevokePayload(TypedDict):
+    subscription: BaseSubscription[ClientCondition]
+    event: UserAuthorizationRevokeEvent
+
+
+class UserUpdateEvent(TypedDict):
+    user_id: str
+    user_login: str
+    user_name: str
+    email: NotRequired[str]
+    email_verified: bool
+    description: str
+
+
+class UserUpdatePayload(TypedDict):
+    subscription: BaseSubscription[UserCondition]
+    event: UserUpdateEvent
+
+
+class WhisperContent(TypedDict):
+    text: str
+
+
+class UserWhisperEvent(TypedDict):
+    from_user_id: str
+    from_user_login: str
+    from_user_name: str
+    to_user_id: str
+    to_user_login: str
+    to_user_name: str
+    whisper_id: str
+    whisper: WhisperContent
+
+
+class UserWhisperPayload(TypedDict):
+    subscription: BaseSubscription[UserCondition]
+    event: UserWhisperEvent
