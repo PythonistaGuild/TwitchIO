9fd2c684b87a357aa68136b9a9e009f5df304300
Eviee Py <mysteapy@gmail.com>
2024-10-12 12:28:05 +1000

    Various docs fixes.

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -1,7 +1,21 @@
-const tables = document.querySelectorAll('.py-attribute-table[data-move-to-id]');
-tables.forEach(table => {
-  let element = document.getElementById(table.getAttribute('data-move-to-id'));
-  let parent = element.parentNode;
-  // insert ourselves after the element
-  parent.insertBefore(table, element.nextSibling);
-});
+const tables = document.querySelectorAll(
+	".py-attribute-table[data-move-to-id]",
+);
+for (const table of tables) {
+	const element = document.getElementById(
+		table.getAttribute("data-move-to-id"),
+	);
+	const parent = element.parentNode;
+
+	// insert ourselves after the element
+	parent.insertBefore(table, element.nextSibling);
+}
+
+const pres = document.querySelectorAll("pre");
+for (const pre_ of pres) {
+	pre_.classList.add("admonition");
+	pre_.insertAdjacentHTML(
+		"afterbegin",
+		`<span class="admonition-title codeB">Code</span>`,
+	);
+}
