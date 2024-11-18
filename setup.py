a25d91e211d02b99599366a3f51167b55f779c5b
Eviee Py <29671945+EvieePy@users.noreply.github.com>
2024-11-18 18:52:16 +1000

    More version specifier fixes.

--- setup.py
+++ setup.py
@@ -1,6 +1,31 @@
+import re
+
 from setuptools import setup  # type: ignore
 
-from twitchio._version import _get_version
+
+def get_version() -> str:
+    version = ""
+    with open("twitchio/__init__.py") as f:
+        match = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE)
+
+    if not match or not match.group(1):
+        raise RuntimeError("Version is not set")
+
+    version = match.group(1)
+
+    if version.endswith(("dev", "a", "b", "rc")):
+        # append version identifier based on commit count
+        try:
+            import subprocess
+
+            p = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, _ = p.communicate()
+            if out:
+                version += out.decode("utf-8").strip()
+        except Exception:
+            pass
+
+    return version
 
 
-setup(version=_get_version(with_hash=False))
+setup(version=get_version())
--- twitchio/__main__.py
+++ twitchio/__main__.py
@@ -24,11 +24,10 @@ SOFTWARE.
 
 import argparse
 import platform
+import re
 import sys
 
 import aiohttp
-
-from ._version import _get_version
 
 
 try:
@@ -52,11 +51,40 @@ parser.add_argument("--version", action="store_true", help="Get version and debu
 args = parser.parse_args()
 
 
+def get_version() -> str:
+    version = ""
+    with open("twitchio/__init__.py") as f:
+        match = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE)
+
+    if not match or not match.group(1):
+        raise RuntimeError("Version is not set")
+
+    version = match.group(1)
+
+    if version.endswith(("dev", "a", "b", "rc")):
+        # append version identifier based on commit count
+        try:
+            import subprocess
+
+            p = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, _ = p.communicate()
+            if out:
+                version += out.decode("utf-8").strip()
+            p = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            out, _ = p.communicate()
+            if out:
+                version += "+g" + out.decode("utf-8").strip()
+        except Exception:
+            pass
+
+    return version
+
+
 def version_info() -> None:
     python_info = "\n".join(sys.version.split("\n"))
 
     info: str = f"""
-    twitchio : {_get_version()}
+    twitchio : {get_version()}
     aiohttp  : {aiohttp.__version__}
 
     Python:
