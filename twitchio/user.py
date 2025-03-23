35cb0b8f8c836e3100828c17baa5833734811bcf
chillymosh <86857777+chillymosh@users.noreply.github.com>
2025-03-23 20:26:23 +0000

    Allow message and sender to be positional
    Allow message and sender to be positional and add extra clarity in the docs.

--- twitchio/user.py
+++ twitchio/user.py
@@ -1121,15 +1121,17 @@ class PartialUser:
 
     async def send_message(
         self,
-        *,
-        sender: str | int | PartialUser,
         message: str,
+        sender: str | int | PartialUser,
+        *,
         token_for: str | PartialUser | None = None,
         reply_to_message_id: str | None = None,
     ) -> SentMessage:
         """|coro|
 
         Send a message to the broadcaster's chat room.
+
+        The PartialUser/User object this method is called on is the broadcaster / channel the message will be sent to.
 
         .. important::
             Requires an app access token or user access token that includes the ``user:write:chat`` scope.
@@ -1147,13 +1149,13 @@ class PartialUser:
 
         Parameters
         ----------
-        sender: str | int | PartialUser
-            The ID, or PartialUser, of the user sending the message. This ID must match the user ID in the user access token.
         message: str
             The message to send. The message is limited to a maximum of 500 characters.
             Chat messages can also include emoticons. To include emoticons, use the name of the emote.
             The names are case sensitive. Don't include colons around the name e.g., `:bleedPurple:`.
             If Twitch recognizes the name, Twitch converts the name to the emote before writing the chat message to the chat room
+        sender: str | int | PartialUser
+            The ID, or PartialUser, of the user sending the message. This ID must match the user ID in the user access token.
         token_for: str | PartialUser | None
             User access token that includes the ``user:write:chat`` scope.
             You can use an app access token which additionally requires ``user:bot scope`` from chatting user, and either ``channel:bot scope`` from broadcaster or moderator status.
