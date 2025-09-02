89ccf72c0a7351d82309e5edd7c0eadaa80a6857
EvieePy <29671945+EvieePy@users.noreply.github.com>
2025-09-02 15:24:12 +1000

    Add basic image and audio support. Add format spec to EventOverlay. Add other properties and methods

--- twitchio/ext/overlays/core.py
+++ twitchio/ext/overlays/core.py
@@ -29,13 +29,15 @@ import copy
 import html
 import importlib.resources
 import json
+import logging
 import struct
 import zlib
+from types import MappingProxyType
 from typing import TYPE_CHECKING, Any, ClassVar, Literal, Self, overload
 
-from twitchio import Colour
+from twitchio.utils import MISSING, Colour, clamp
 
-from .enums import Animation, AnimationSpeed, EventPosition, Font
+from .enums import Alignment, Animation, AnimationSpeed, EventPosition, Font
 from .exceptions import AudioLoadedError, BlueprintError
 
 
@@ -44,10 +46,13 @@ if TYPE_CHECKING:
 
     from aiohttp.web import WebSocketResponse
 
-    from .types_ import OverlayEventT, OverlayPartT
+    from .types_ import ImageDimensions, OverlayEventT, OverlayPartT
 
 
 __all__ = ("Overlay", "OverlayEvent", "PlayerOverlay")
+
+
+LOGGER: logging.Logger = logging.getLogger(__name__)
 
 
 class OverlayEvent:
@@ -57,11 +62,25 @@ class OverlayEvent:
         self._raw: OverlayEventT = {
             "parts": [],
             "audio": "",
+            "volume": 0.5,
             "duration": 5000,
             "duration_is_audio": False,
             "force_override": False,
             "stack_event": False,
+            "animation": None,
+            "speed": None,
         }
+
+    def __repr__(self) -> str:
+        return (
+            f"OverlayEvent(parts={len(self._raw['parts'])}, duration={self._raw['duration']}, volume={self._raw['volume']})"
+        )
+
+    def __format__(self, format_spec: str) -> str:
+        if format_spec in ("bp", "template"):
+            return self.to_blueprint()
+
+        return repr(self)
 
     def escape(self, value: str, /) -> str:
         """Method which escapes the provided the content via the ``html`` standard library.
@@ -78,11 +97,88 @@ class OverlayEvent:
         """
         return html.escape(value)
 
+    def reset(self) -> Self:
+        """Method to reset this event back to it's default state.
+
+        Returns
+        -------
+        OverlayEvent
+            This method returns ``self`` which allows for fluid-style chaining.
+        """
+        self._raw = {
+            "parts": [],
+            "audio": "",
+            "volume": 0.5,
+            "duration": 5000,
+            "duration_is_audio": False,
+            "force_override": False,
+            "stack_event": False,
+            "animation": None,
+            "speed": None,
+        }
+        return self
+
+    def set_properties(
+        self,
+        *,
+        animation: Animation = MISSING,
+        animation_speed: AnimationSpeed = MISSING,
+        override: bool = MISSING,
+        stack: bool = MISSING,
+        duration: int = MISSING,
+        volume: int = MISSING,
+    ) -> Self:
+        """Method which allows setting various properties on the overlay event.
+
+        Any argument not provided will be ignored.
+
+        Parameters
+        ----------
+        animation: ...
+            ...
+        animation_speed: ...
+            ...
+        override: ...
+            ...
+        stack: ...
+            ...
+        duration: ...
+            ...
+        volume: ...
+            ...
+
+        Returns
+        -------
+        OverlayEvent
+            This method returns ``self`` which allows for fluid-style chaining.
+        """
+        if animation is not MISSING:
+            self._raw["animation"] = f"animate__{animation.value}"
+
+        if animation_speed is not MISSING:
+            self._raw["speed"] = f"animate__{animation_speed.value}"
+
+        if override is not MISSING:
+            self._raw["force_override"] = override
+
+        if stack is not MISSING:
+            self._raw["stack_event"] = stack
+
+        if duration is not MISSING:
+            self._raw["duration"] = duration
+
+        if volume is not MISSING:
+            vol = clamp(volume, 0, 100) / 100
+            self._raw["volume"] = vol
+
+        return self
+
     def add_html(
         self,
         html: str,
         animation: Animation | None = None,
         animation_speed: AnimationSpeed | None = None,
+        alignment: Alignment | None = None,
     ) -> Self:
         """Method to add a custom HTML segment to this event.
 
@@ -112,7 +208,12 @@ class OverlayEvent:
         """
         middle = f" animate__{animation.value}" if animation else ""
         speed = f" animate__{animation_speed.value}" if animation_speed else ""
-        part: OverlayPartT = {"content": html, "animation": middle, "speed": speed}
+        part: OverlayPartT = {
+            "content": html,
+            "animation": middle,
+            "speed": speed,
+            "alignment": f" {alignment.value}" if alignment else None,
+        }
 
         self._raw["parts"].append(part)
         return self
@@ -126,6 +227,7 @@ class OverlayEvent:
         font: Font | None = None,  # TODO: Default Font...
         size: int = 24,
         colour: Colour | None = None,
+        alignment: Alignment | None = None,
     ) -> Self:
         """Method to add custom text content as a segement to this event.
 
@@ -163,14 +265,63 @@ class OverlayEvent:
         speed = f" animate__{animation_speed.value}" if animation_speed else ""
         col = colour or Colour.from_hex("#000000")
 
-        part: OverlayPartT = {"content": escaped, "animation": middle, "speed": speed, "size": size, "colour": col.html}
+        part: OverlayPartT = {
+            "content": escaped,
+            "animation": middle,
+            "speed": speed,
+            "size": size,
+            "colour": col.html,
+            "alignment": f" {alignment.value}" if alignment else None,
+        }
         self._raw["parts"].append(part)
 
         return self
 
-    def add_image(self) -> Self: ...
+    def add_image(
+        self,
+        identifier: str,
+        /,
+        animation: Animation | None = None,
+        animation_speed: AnimationSpeed | None = None,
+        dimensions: ImageDimensions | None = None,
+        alignment: Alignment | None = None,
+    ) -> Self:
+        middle = f" animate__{animation.value}" if animation else ""
+        speed = f" animate__{animation_speed.value}" if animation_speed else ""
+        part: OverlayPartT = {
+            "content": identifier,
+            "is_image": True,
+            "animation": middle,
+            "speed": speed,
+            "dimensions": dimensions,
+            "alignment": f" {alignment.value}" if alignment else None,
+        }
+        self._raw["parts"].append(part)
 
-    def set_audio(self, name: str, /) -> Self: ...
+        return self
+
+    def set_audio(self, identifier: str, /, *, volume: int = 50) -> Self:
+        """Method which sets the audio for this overlay event.
+
+        Parameters
+        ----------
+        identifier: str
+            The audio identifier, which should be a unique identifier which was used in
+            :meth:`~twitchio.ext.overlays.Overlay.mount_audio`.
+        volume: int
+            An optional :class:`int` which sets the volume of the audio. The volume will be clamped between ``0`` and ``100``.
+
+        Returns
+        -------
+        OverlayEvent
+            This method returns ``self`` which allows for fluid-style chaining.
+        """
+        vol = clamp(volume, 0, 100) / 100
+
+        self._raw["audio"] = identifier
+        self._raw["volume"] = vol
+
+        return self
 
     def set_duration(self, value: int | None = 5000, *, as_audio: bool = False) -> Self:
         """Method which sets the overall duration of this overlay event.
@@ -196,6 +347,40 @@ class OverlayEvent:
 
         return self
 
+    def set_stacking(self, value: bool, /) -> Self:
+        """Method which sets whether or not this overlay event should stack below the previous event.
+
+        Parameters
+        ----------
+        value: bool
+            If set to ``True``, the overlay event will stack below other events. Defaults to ``False``.
+
+        Returns
+        -------
+        OverlayEvent
+            This method returns ``self`` which allows for fluid-style chaining.
+        """
+        self._raw["stack_event"] = value
+        return self
+
+    def set_override(self, value: bool, /) -> Self:
+        """Method which sets whether ot not this overlay event should override any previous events.
+
+        Setting this to ``True`` will ensure any previous events are removed.
+
+        Parameters
+        ----------
+        value: bool
+            If set to ``True``, all previous events will be removed when this event is triggered.
+
+        Returns
+        -------
+        OverlayEvent
+            This method returns ``self`` which allows for fluid-style chaining.
+        """
+        self._raw["force_override"] = value
+        return self
+
     @overload
     def _compress(self, convert: Literal[True] = True) -> str: ...
 
@@ -218,6 +403,20 @@ class OverlayEvent:
 
     @classmethod
     def from_blueprint(cls, template_string: str, /) -> Self:
+        """Classmethod which loads and returns a previous designed event from a Blueprint string.
+
+        You can generate a blueprint for an event via :meth:`.to_blueprint`.
+
+        Parameters
+        ----------
+        template_string: str
+            The previous saved blueprint string to load an event from.
+
+        Returns
+        -------
+        OverlayEvent
+            This method returns ``self`` which allows for fluid-style chaining.
+        """
         decoded = base64.b64decode(template_string)
         version = struct.unpack(">H", decoded[:2])
 
@@ -236,6 +435,15 @@ class OverlayEvent:
         return inst
 
     def to_blueprint(self) -> str:
+        """Method which generates and returns a blueprint string, which can be saved and used later to load an event.
+
+        You can load an event from a blueprint via :meth:`.from_blueprint`.
+
+        Returns
+        -------
+        str
+            The event in the form of a compressed string, which can easily be saved and loaded later.
+        """
         return self._compress()
 
     def as_dict(self) -> OverlayEventT:
@@ -283,45 +491,16 @@ class Overlay:
     def __init__(self, *, secret: str) -> None:
         self._secret = secret
         self._position = EventPosition.center
-        self._audio_paths: dict[str, os.PathLike[str]] = {}
+        self._audio_paths: dict[str, os.PathLike[str] | str] = {}
+        self._image_paths: dict[str, os.PathLike[str] | str] = {}
 
     @property
     def secret(self) -> str:
+        """Property returning the overlay secret.
+
+        The secret is used to generate a URL on the adapter to access the overlay.
+        """
         return self._secret
-
-    async def setup(self) -> str | None: ...
-
-    async def close(self) -> str | None:
-        for id_, sock in self.__sockets__.items():
-            try:
-                await sock.close()
-            except Exception:
-                # TODO: Logging...
-                pass
-
-            self.__sockets__.pop(id_, None)
-
-    async def trigger(self, event: OverlayEvent, *, skip_queue: bool = False) -> None:
-        # TODO: Check connected?
-        if not isinstance(event, OverlayEvent):  # pyright: ignore[reportUnnecessaryIsInstance]
-            raise TypeError(f"Expected OverlayEvent or derivative for {self!r} trigger, got {event!r}.")
-
-        # TODO: Skip Queue...
-        await self._push(event)
-
-    async def _push(self, event: OverlayEvent) -> None:
-        for id, sock in self.__sockets__.copy().items():
-            if sock.closed:
-                self.__sockets__.pop(id, None)
-
-            data = {"eventData": event.as_dict(), "position": self._position.value}
-            try:
-                await sock.send_json(data)
-            except Exception:
-                # TODO: Logging...
-                pass
-
-    def connected(self) -> bool: ...
 
     @property
     def template(self) -> str:
@@ -339,39 +518,157 @@ class Overlay:
     def title(self) -> str:
         return self.__title__
 
-    def set_template(self, path: os.PathLike[str] | str, *, content: str | None = None) -> None:
+    @property
+    def position(self) -> EventPosition:
+        return self._position
+
+    @property
+    def audio(self) -> MappingProxyType[str, os.PathLike[str] | str]:
+        return MappingProxyType(self._audio_paths)
+
+    @property
+    def images(self) -> MappingProxyType[str, os.PathLike[str] | str]:
+        return MappingProxyType(self._image_paths)
+
+    async def setup(self) -> None:
+        """|coro|
+
+        Method which allows custom asynchronous setup to occur on the overlay.
+
+        This method is called once when the overlay is added to an adapter.
+
+        Should be overriden if async setup is required.
+        """
+        ...
+
+    async def teardown(self) -> None:
+        """|coro|
+
+        Method which allows custom asynchronous cleanup to occur on the overlay.
+
+        This method is called once when the overlay is removed from an adapter.
+
+        Should be overriden if async cleanup is required.
+        """
+        ...
+
+    async def close(self) -> None:
+        """|coro|
+
+        Method which performs cleanup on the overlay and gracefully closes any active connections.
+
+        This method could be overriden, however consider using :meth:`.teardown` instead.
+
+        The default implementation of this method should be called if overriden, e.g. ``await super().close()``.
+        """
+        for id_, sock in self.__sockets__.items():
+            try:
+                await sock.close()
+            except Exception as e:
+                LOGGER.debug("Exception occurred duting cleanup on websocket '%s' on '%r': %s", id_, self, e)
+
+            self.__sockets__.pop(id_, None)
+
+    async def trigger(self, event: OverlayEvent, /) -> None:
+        # TODO: Check connected?
+        if not isinstance(event, OverlayEvent):  # pyright: ignore[reportUnnecessaryIsInstance]
+            raise TypeError(f"Expected OverlayEvent or derivative for {self!r} trigger, got {event!r}.")
+
+        await self._push(event)
+
+    async def _push(self, event: OverlayEvent) -> None:
+        for id_, sock in self.__sockets__.copy().items():
+            if sock.closed:
+                self.__sockets__.pop(id_, None)
+
+            data = {"eventData": event.as_dict(), "position": self._position.value}
+
+            try:
+                await sock.send_json(data)
+            except Exception as e:
+                LOGGER.warning("Exception occurred while sending data to overlay websocket '%s' on '%r': %s", id_, self, e)
+
+    def connected(self) -> bool: ...
+
+    def set_template(self, path: os.PathLike[str] | str, *, content: str | None = None) -> Self:
+        """Method which sets the default HTML template used by the overlay.
+
+        Returns
+        -------
+        Overlay
+            This method returns ``self`` which allows for fluid-style chaining.
+        """
+        # TODO: Explain Templates...
+        # TODO: Explain default template
         if content:
             self.__template__ = content
-            return
+            return self
 
         with open(path) as fp:
             self.__template__ = fp.read()
 
-    def set_stylesheet(self, path: os.PathLike[str] | str, *, content: str | None = None) -> None:
+        return self
+
+    def set_stylesheet(self, path: os.PathLike[str] | str, *, content: str | None = None) -> Self:
         if content:
             self.__stylesheet__ = content
-            return
+            return self
 
         with open(path) as fp:
             self.__stylesheet__ = fp.read()
 
-    def set_javascript(self, path: os.PathLike[str] | str, *, content: str | None = None) -> None:
+        return self
+
+    def set_javascript(self, path: os.PathLike[str] | str, *, content: str | None = None) -> Self:
         if content:
             self.__javascript__ = content
-            return
+            return self
 
         with open(path) as fp:
             self.__javascript__ = fp.read()
+
+        return self
 
     def set_position(self, position: EventPosition, /) -> Self:
         self._position = position
         return self
 
-    async def mount_audio(self, name: str, *, path: os.PathLike[str]) -> ...:
-        if name in self._audio_paths:
-            raise AudioLoadedError(f"Audio with the name '{name}' has already been added to {self!r}.")
+    def mount_audio(self, identifier: str, *, path: os.PathLike[str] | str) -> Self:
+        if identifier in self._audio_paths:
+            raise AudioLoadedError(f"Audio with the identifier '{identifier}' has already been added to {self!r}.")
 
-        self._audio_paths[name] = path
+        self._audio_paths[identifier] = path
+        return self
+
+    def unmount_audio(self, identifier: str, /) -> Self:
+        if identifier not in self._audio_paths:
+            raise AudioLoadedError(f"Audio with the identifier '{identifier}' has not been previously loaded to {self!r}.")
+
+        del self._audio_paths[identifier]
+        return self
+
+    def get_audio(self, identifier: str, /) -> os.PathLike[str] | str | None:
+        return self._audio_paths.get(identifier)
+
+    # ...
+    # TODO: Univieral Audio/Image Error?
+
+    def mount_image(self, identifier: str, *, path: os.PathLike[str] | str) -> Self:
+        if identifier in self._image_paths:
+            raise AudioLoadedError(f"Image with the identifier '{identifier}' has already been added to {self!r}.")
+
+        self._image_paths[identifier] = path
+        return self
+
+    def unmount_image(self, identifier: str, /) -> Self:
+        if identifier not in self._image_paths:
+            raise AudioLoadedError(f"Image with the identifier '{identifier}' has not been previously loaded to {self!r}.")
+
+        del self._image_paths[identifier]
+        return self
+
+    def get_image(self, identifier: str, /) -> os.PathLike[str] | str | None:
+        return self._image_paths.get(identifier)
 
     def generate_html(self) -> str:
         template = self.template
--- twitchio/ext/overlays/static/scripts.js
+++ twitchio/ext/overlays/static/scripts.js
@@ -33,17 +33,98 @@ async function prepareData(data) {
     const element = document.createElement("div");
     element.classList.add("eventContainer");
 
+    const anim = data.animation;
+    const speed = data.speed;
+
+    if (anim) {
+        element.classList.add("animate__animated", anim);
+    }
+    if (speed) {
+        element.classList.add(speed);
+    }
+
     for (const part of data.parts) {
+        let img, iw, ih;
+
+        const isImg = part.is_image;
         const content = part.content;
         const anim = part.animation || "";
         const speed = part.speed || "";
         const fontSize = part.size || 22;
+        const colour = part.colour || "#000";
+        const align = part.alignment || "s";
 
-        let html = `<span style="font-size: ${fontSize}px;" class="animate__animated animate__infinite${anim}${speed}">${content}</span>`;
+        if (isImg) {
+            img = await prepareImg(part.content);
+        }
+
+        if (img) {
+            if (part.dimensions) {
+                [iw, ih] = part.dimensions;
+                img.style.width = `${iw}px`;
+                img.style.height = `${ih}px`;
+            }
+
+            img.classList.add("animate__animated");
+            anim ? img.classList.add(anim.trim()) : null;
+            speed ? img.classList.add(speed.trim()) : null;
+            align ? img.classList.add(align.trim()) : null;
+
+            element.insertAdjacentElement("beforeend", img);
+            continue;
+        }
+
+        let html = `<span 
+                        style="font-size: ${fontSize}px;color: ${colour};"
+                        class="animate__animated animate__infinite${anim}${speed}${align}"
+                    >
+                        ${content}
+                    </span>`;
         element.insertAdjacentHTML("beforeend", html);
     }
 
     return element;
+}
+
+async function prepareAudio(uri) {
+    if (!uri) { return null }
+    let url;
+
+    try {
+        // Check if audio is an exisiting URL...
+        url = new URL(uri);
+    } catch (_) {
+        url = new URL(`${window.location}/audio/${uri}`);
+    }
+
+    if (url.protocol !== "http:" && url.protocol !== "https:") {
+        return null
+    }
+
+    const audio = new Audio(url);
+    return audio;
+}
+
+async function prepareImg(uri) {
+    if (!uri) { return null }
+    let url;
+
+    try {
+        // Check if image is an exisiting URL...
+        url = new URL(uri);
+    } catch (_) {
+        url = new URL(`${window.location}/image/${uri}`);
+    }
+
+    if (url.protocol !== "http:" && url.protocol !== "https:") {
+        return null
+    }
+
+    const img = document.createElement("img");
+    img.src = url.href;
+    img.classList.add("eventImg");
+
+    return img;
 }
 
 async function displayData(data) {
@@ -52,20 +133,33 @@ async function displayData(data) {
 
     const prepared = await prepareData(event);
     const wrapper = document.getElementById("wrapper");
-    
-    wrapper.classList.remove(wrapper.classList.name);
-    wrapper.classList.add(data.position);
-
     if (!wrapper) {
         console.error(`The <div> with the ID of "wrapper" cannot be found.`);
         return;
     }
+
+    wrapper.classList.remove(wrapper.classList.name);
+    wrapper.classList.add(data.position);
 
     if (event.force_override) {
         await cleanState();
     }
 
     wrapper.insertAdjacentElement("afterbegin", prepared);
+
+    const audio = await prepareAudio(event.audio);
+    if (audio) {
+        audio.volume = event.volume || 50;
+        audio.play();
+
+        if (event.duration_is_audio) {
+            audio.addEventListener("ended", async () => {
+                await endEvent(prepared);
+            });
+
+            return;
+        }
+    }
 
     if (event.duration !== null && event.duration > 0) {
         setTimeout(async () => {
